cfddbce86aac8ca57d660fdadbb9215c6e7ad5b1
Axelg728 <axelgabrielg7@gmail.com>
2025-12-16 18:55:17 -0600

    actualizado con apk

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'planifica-estudio',
-  webDir: 'www'
-};
-
-export default config;
